6081ca4e4ff2fd3e56b20da4d9a59677e99a3d3e
cfowl <goose_7@msn.com>
2022-02-12 11:17:05 -0700

    all,active,completed buttons working

--- week6/main.js
+++ week6/main.js
@@ -4,7 +4,22 @@ const localStorageName = 'todoList';
 
 let todoArray = [];
 
-loadList();
+let selection = 'all';
+
+loadList(selection);
+
+document.getElementById('all-btn').addEventListener('click', () => {
+    selection = 'all';
+    loadList(selection);
+});
+document.getElementById('active-btn').addEventListener('click', () => {
+    selection = 'active';
+    loadList(selection);
+});
+document.getElementById('completed-btn').addEventListener('click', () => {
+    selection = 'completed';
+    loadList(selection);
+});
 
 document.forms['addTodo'].addEventListener('submit', addNewTodo);
 
@@ -20,15 +35,25 @@ function addNewTodo(event) {
     saveList();
 }
 
-function saveList() {
-    localStorage.setItem(localStorageName, JSON.stringify(todoArray));
-    displayList();
+function deleteTodo(i, event) {
+    event.preventDefault();
+    todoArray.splice(i, 1);
+    saveList();
 }
 
-function loadList() {
+function saveList() {
+    localStorage.setItem(localStorageName, JSON.stringify(todoArray));
+    loadList(selection);
+}
+
+function loadList(selection) {
     const savedTodoArray = JSON.parse(localStorage.getItem(localStorageName));
     if(savedTodoArray !== null) todoArray = savedTodoArray;
-    if(todoArray.length !== 0) displayList();
+    if(todoArray.length !== 0) {
+        if(selection === 'active') displayActiveList();
+        else if(selection === 'completed') displayCompletedList();
+        else displayList();
+    }
 }
 
 function displayList() {
@@ -73,10 +98,85 @@ function displayList() {
     }
 }
 
-function deleteTodo(i, event) {
-    event.preventDefault();
-    todoArray.splice(i, 1);
-    saveList();
+function displayActiveList() {
+    console.log('active list function');
+
+    const todoList = document.getElementById('todoList');
+    todoList.innerHTML = '';
+    for(let i=0; i< todoArray.length; i++) {
+        
+        if(!todoArray[i].completed) {
+            const li = document.createElement('li');
+            li.classList.add('item');
+            const check = todoArray[i].completed ? 'checked' : 'unchecked';
+            li.innerHTML = `<label for'${todoArray[i].id}'><input type='checkbox' id='${todoArray[i].id}' ${check}>${todoArray[i].content}</label>`;
+            const button = document.createElement('button');
+            button.classList.add('delete-btn');
+            button.textContent = "X";
+
+            // delete button event listener
+            button.addEventListener('click', event => {
+                deleteTodo(i, event);
+            });
+
+            li.appendChild(button);
+            todoList.appendChild(li);
+
+            const label = li.firstElementChild;
+
+            // checkmark event listener
+            label.addEventListener('click', event => {
+                addRemoveCheck(i, event);
+            });
+        }
+    }
+
+}
+
+function displayCompletedList() {
+    console.log('completed list function');
+
+    const todoList = document.getElementById('todoList');
+    todoList.innerHTML = '';
+    for(let i=0; i< todoArray.length; i++) {
+        
+        if(todoArray[i].completed) {
+            const li = document.createElement('li');
+            li.classList.add('item');
+            const check = todoArray[i].completed ? 'checked' : 'unchecked';
+            li.innerHTML = `<label for'${todoArray[i].id}'><input type='checkbox' id='${todoArray[i].id}' ${check}>${todoArray[i].content}</label>`;
+            const button = document.createElement('button');
+            button.classList.add('delete-btn');
+            button.textContent = "X";
+
+            // delete button event listener
+            button.addEventListener('click', event => {
+                deleteTodo(i, event);
+            });
+
+            li.appendChild(button);
+            todoList.appendChild(li);
+
+            const label = li.firstElementChild;
+
+            // checkmark event listener
+            label.addEventListener('click', event => {
+                addRemoveCheck(i, event);
+            });
+
+            // strike through text if checked and turn button red on hover
+            if(todoArray[i].completed) {
+                label.style.textDecoration = 'line-through';
+                button.style.backgroundColor = 'orange';
+                button.addEventListener('mouseover', () => {
+                    button.style.backgroundColor = 'red';
+                });
+                button.addEventListener('mouseleave', () => {
+                    button.style.backgroundColor = 'orange';
+                });
+            }
+        }
+    }
 }
 
 function addRemoveCheck(i, event) {
