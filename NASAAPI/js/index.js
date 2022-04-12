54bea41a981ef20a90bf8d5b78719010a0e01570
cfowl <goose_7@msn.com>
2022-04-11 21:58:39 -0600

    reset input value

--- NASAAPI/js/index.js
+++ NASAAPI/js/index.js
@@ -15,6 +15,7 @@ const form = document.forms['search-form'];
 form.addEventListener('submit', event => {
     event.preventDefault();
     const keyword = formatKeyword(form.keyword.value);
+    form.keyword.value = '';
     const mediaType = form.mediaType.value;
 
     // create controller and initialize it
